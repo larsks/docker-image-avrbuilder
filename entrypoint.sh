258e7a30dd91f55bb90e4a86c5e30b1332ae436f
Lars Kellogg-Stedman <lars@oddbit.com>
2019-02-10 21:51:51 -0500

    more verbose message on console

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,16 @@
 IPADDR=$(ip addr show eth0 | sed -n 's/.*inet \([^ /]*\).*/\1/p')
 
 if [ "$1" == "docs" ]; then
-	printf "http://%s/\\n" $IPADDR
+	cat <<-EOF
+	======================================================================
+
+	To view AVR-related documentation, point your browser at the
+	following link:
+
+	    http://${IPADDR}/
+
+	======================================================================
+	EOF
 	exec darkhttpd /usr/share/avr-docs > /dev/null
 fi
 
